89ddeab918bd924d9b762290ff51516f77006782
jeffmoger <jeff-github@moger.com>
2020-06-05 15:10:20 -0700

    Added call to api to get data onClick

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,36 +1,36 @@
 import React from "react";
-import { useAuth } from "../context/auth";
 import Nav from '../components/Nav'
 
 function Settings(props) {
-  const { setAuthTokens } = useAuth();
   const { token } = JSON.parse(localStorage.getItem("tokens"));
-  console.log(token)
+
+  function handleClick() {
+    refreshDataWithGoogle(token)
+    .then(response => {
+      localStorage.setItem('changes', JSON.stringify(response));
+    })
+  }
+  
   
   return (
     <div id="main-container" className="eleven columns" style={{marginTop: '5%'}}>
       <Nav />
       <h3>Settings</h3>
       <p>Manage data sync with Google here.</p>
-      <button>Refresh Data</button>
+      <button onClick={handleClick}>Refresh Data</button>
     </div>
     )
 }
 
 
-async function refreshDataWithGoogle(email, password) {
+async function refreshDataWithGoogle(token) {
   try {
-    const r = await fetch('/api/users/login', {
-        method: 'POST',
+    const r = await fetch('/api/data', {
+        method: 'GET',
         headers: {
           'content-type': 'application/json',
+          'authorization': 'Token ' + token
         },
-        body: JSON.stringify({
-          "user": {
-            "email": email,
-            "password": password
-          }
-        }),
       });
     const response = await r.json();
     return response;
