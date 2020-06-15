e5e9db587f13db1cdf5eaa547c4042c98719601b
jeffmoger <jeff-github@moger.com>
2020-06-15 14:08:16 -0700

    url in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer style={{position:"fixed",bottom:0, width:"100%"}}>
-      <a href="#">moger.com</a>
+      <a href="https://moger.com">moger.com</a>
     </footer>
   )
 }
