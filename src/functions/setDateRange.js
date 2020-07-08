4a49d34fd94010b5878c1e681a9b7e509d08fa7e
jeffmoger <jeff-github@moger.com>
2020-07-07 22:10:50 -0700

    lint only

--- src/functions/setDateRange.js
+++ src/functions/setDateRange.js
@@ -1,12 +1,12 @@
 import moment from 'moment';
 
-export default function setDateRange(days){
-    let end = moment().startOf('day');
-    end.add(1, 'days');
-    let start = moment().startOf('day');
-    start.subtract(days, 'days');
-    const rangeString = start.format('lll') + ' until ' + end.format('lll');
-    const intStart = parseInt(start.format('x'), 10);
-    const intEnd = parseInt(end.format('x'), 10);
-    return [ intStart, intEnd, rangeString ]
+export default function setDateRange(days) {
+  let end = moment().startOf('day');
+  end.add(1, 'days');
+  let start = moment().startOf('day');
+  start.subtract(days, 'days');
+  const rangeString = start.format('lll') + ' until ' + end.format('lll');
+  const intStart = parseInt(start.format('x'), 10);
+  const intEnd = parseInt(end.format('x'), 10);
+  return [intStart, intEnd, rangeString];
 }
