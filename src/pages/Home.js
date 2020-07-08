0a9e1dd3a7d4d75abcc5736db7bb6bc464559507
jeffmoger <jeff-github@moger.com>
2020-07-07 20:47:03 -0700

    Home now redirects to login when not authenticated.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { useAuth } from '../context/auth';
 import HomeCharts from '../components/HomeCharts';
 
 export default function Home(props) {
   const isAuthenticated = useAuth();
-  const notAuthMessage = (
-    <>
-      <h3>Home</h3>
-      <p>
-        You must <Link to="/login">login</Link> to use this site.
-      </p>
-    </>
-  );
   return (
     <main>
-      {isAuthenticated.authTokens ? <HomeCharts /> : notAuthMessage}
-      <div id="data"></div>
+      {isAuthenticated.authTokens ? (
+        <HomeCharts />
+      ) : (
+        <Redirect to={'/settings'} />
+      )}
     </main>
   );
 }
