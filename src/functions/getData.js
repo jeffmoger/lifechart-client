8a9cdded69a985d0ff6eb66c97581e252485f451
jeffmoger <jeff-github@moger.com>
2020-07-08 14:04:24 -0700

    linting only

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -1,18 +1,17 @@
 export default async function getData(id, token) {
-    try {
-      const r = await fetch(`${process.env.REACT_APP_API}/api/get_range_data`, {
-          method: 'GET',
-          headers: {
-            'content-type': 'application/json',
-            'authorization': 'Token ' + token,
-            'id': id
-          },
-        });
-      const response = await r.json();
-      return response;
-    }
-    catch (err) {
-      console.log(err)
-      return err;
-    }
+  try {
+    const r = await fetch(`${process.env.REACT_APP_API}/api/get_range_data`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        authorization: 'Token ' + token,
+        id: id,
+      },
+    });
+    const response = await r.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+    return err;
   }
+}
