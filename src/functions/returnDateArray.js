4c33c7e870404111107c46e02e77d89055c6179b
jeffmoger <jeff-github@moger.com>
2020-07-07 21:14:00 -0700

    linting only

--- src/functions/returnDateArray.js
+++ src/functions/returnDateArray.js
@@ -1,10 +1,14 @@
 import moment from 'moment';
 
 export default function returnDateArray(dateRangeArray) {
-    const [ start, end, dateRange ] = dateRangeArray
-    console.log(dateRange)
-    for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
-      arr.push(parseInt(moment(dt).format('x')));
-    }
-    return arr;
-  };
+  const [start, end, dateRange] = dateRangeArray;
+  console.log(dateRange);
+  for (
+    var arr = [], dt = new Date(start);
+    dt <= end;
+    dt.setDate(dt.getDate() + 1)
+  ) {
+    arr.push(parseInt(moment(dt).format('x')));
+  }
+  return arr;
+}
