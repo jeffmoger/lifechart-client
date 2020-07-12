49b5a4518b7421227a13151d23e84b111651c29f
jeffmoger <jeff-github@moger.com>
2020-07-11 22:59:33 -0700

    Empty About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function About(props) {
-  console.log('Rendering About')
   return (
     <main>
-      <Nav />
       <h3>About</h3>
+      <p>Some text here about why I made this site.</p>
     </main>
-  )
+  );
 }
