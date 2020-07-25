09e952b833c99efeac28083cbfc2d0d884820509
jeffmoger <jeff-github@moger.com>
2020-07-25 14:05:28 -0700

    lint only

--- src/functions/handleSync.js
+++ src/functions/handleSync.js
@@ -1,23 +1,22 @@
-import moveDataFromGoogle from './functions/moveDataFromGoogle'
+import moveDataFromGoogle from './functions/moveDataFromGoogle';
 import getData from './functions/getData';
-import loadChartData from './functions/loadChartData'
+import loadChartData from './functions/loadChartData';
 
-function handleSync(id, token){
-    moveDataFromGoogle(id, token)
-    .then(async response => {
+function handleSync(id, token) {
+  moveDataFromGoogle(id, token)
+    .then(async (response) => {
       var dataObject = {};
-      dataObject.syncReport = response
-      dataObject.data = await getData(id, token)
-      return dataObject
+      dataObject.syncReport = response;
+      dataObject.data = await getData(id, token);
+      return dataObject;
     })
-    .then(dataObject => {
+    .then((dataObject) => {
       dataObject.chartData = loadChartData(dataObject.data);
-      dataObject.fetched = new Date().getTime()
+      dataObject.fetched = new Date().getTime();
       //setDisplayResponse(dataObject.syncReport);
       //localStorage.setItem("sync", JSON.stringify(dataObject));
-      return dataObject
-    })
-  }
+      return dataObject;
+    });
+}
 
-  export default handleSync
-
+export default handleSync;
