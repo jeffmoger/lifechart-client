e47e796e0ff1ae370bcfaeffc76fd1a5dfd4b329
jeffmoger <jeff-github@moger.com>
2020-07-25 00:20:05 -0700

    Commented out unused Redirect import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+//import { Redirect } from 'react-router-dom';
 import { useAuth } from '../context/auth';
 import HomeCharts from '../components/HomeCharts';
 import HomeDemo from '../components/HomeDemo';
