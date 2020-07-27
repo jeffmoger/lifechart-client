d82f0b79c07026bc5e196c9ee394fe5592acd6f5
jeffmoger <jeff-github@moger.com>
2020-07-26 22:35:40 -0700

    lint only

--- src/functions/moveDataFromGoogle.js
+++ src/functions/moveDataFromGoogle.js
@@ -1,20 +1,22 @@
 async function moveDataFromGoogle(id, token) {
-    try {
-        const r = await fetch(`${process.env.REACT_APP_API}/api/move_data_from_google`, {
-            method: 'GET',
-            headers: {
-            'content-type': 'application/json',
-            'authorization': 'Token ' + token,
-            'id': id
-            },
-        });
-        const response = await r.json();
-        return response;
-    }
-    catch (err) {
-        console.log(err)
-        return err;
-    }
+  try {
+    const r = await fetch(
+      `${process.env.REACT_APP_API}/api/move_data_from_google`,
+      {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          authorization: 'Token ' + token,
+          id: id,
+        },
+      }
+    );
+    const response = await r.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 }
 
-export default moveDataFromGoogle
+export default moveDataFromGoogle;
