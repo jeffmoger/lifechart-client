b5e9d29770ceb73b41181fe329b0f18374969817
jeffmoger <jeff-github@moger.com>
2020-08-22 14:52:05 -0700

    hid homeDemo call

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useAuth } from '../context/auth';
 import HomeCharts from '../components/HomeCharts';
-import HomeDemo from '../components/HomeDemo';
+//import HomeDemo from '../components/HomeDemo';
 
 export default function Home(props) {
   const isAuthenticated = useAuth();
