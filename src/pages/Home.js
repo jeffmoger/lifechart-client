ab49a91abe2e18ee83473234dae1a4f23dc7999f
jeffmoger <jeff-github@moger.com>
2020-09-09 23:05:28 -0700

    get profile and pass to child components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useAuth } from '../context/auth';
 import HomeCharts from '../components/HomeCharts';
 //import HomeDemo from '../components/HomeDemo';
 
-export default function Home(props) {
+export default function Home() {
   const isAuthenticated = useAuth();
+  const [profile, setProfile] = useState('');
+  const { id, token } = isAuthenticated.authTokens;
+
+  useEffect(() => {
+    async function getProfile() {
+      const r = await fetch(`${process.env.REACT_APP_API}/api/users/read`, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          authorization: 'Token ' + token,
+          id: id,
+        },
+      });
+      const response = await r.json();
+      return response;
+    }
+    getProfile().then((result) => setProfile(result));
+  }, [id, token]);
+
   return (
     <main>
-      {isAuthenticated.authTokens ? <HomeCharts /> : <Redirect to={'/about'} />}
+      {isAuthenticated.authTokens ? (
+        <HomeCharts authTokens={isAuthenticated.authTokens} profile={profile} />
+      ) : (
+        <Redirect to={'/about'} />
+      )}
     </main>
   );
 }
