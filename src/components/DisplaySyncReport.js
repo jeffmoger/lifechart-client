9f96a6d96caaa2ad2cd3cfd3384525f1863d2de9
jeffmoger <jeff-github@moger.com>
2020-09-18 13:51:37 -0700

    lint changes only

--- src/components/DisplaySyncReport.js
+++ src/components/DisplaySyncReport.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const DisplaySyncReport = props => {
-    const { data } = props
-    return (
-      <table>
-        <thead>
-          <tr>
-            <td>Data Source</td>
-            <td>Modified</td>
-            <td>New</td>
-          </tr>
-        </thead>
-        <tbody>
+const DisplaySyncReport = (props) => {
+  const { data } = props;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Data Source</td>
+          <td>Modified</td>
+          <td>New</td>
+        </tr>
+      </thead>
+      <tbody>
         {data.map((dat, index) => (
           <tr key={index}>
-            <td>{dat[0].replace('com.google.','')}</td>
+            <td>{dat[0].replace('com.google.', '')}</td>
             <td>{dat[1]}</td>
             <td>{dat[2]}</td>
           </tr>
         ))}
-        </tbody>
-      </table>
-    );
-  };
+      </tbody>
+    </table>
+  );
+};
 
-export default DisplaySyncReport
+export default DisplaySyncReport;
