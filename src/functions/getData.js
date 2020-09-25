ce0bcea3c7d89e9e45548fe84125808ec8a63830
jeffmoger <jeff-github@moger.com>
2020-09-25 15:10:26 -0700

    removed unnecessary id from headers

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -1,13 +1,15 @@
-export default async function getData(id, token) {
+export default async function getData(token, date_range) {
   try {
-    const r = await fetch(`${process.env.REACT_APP_API}/api/get_range_data`, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        authorization: 'Token ' + token,
-        id: id,
-      },
-    });
+    const r = await fetch(
+      `${process.env.REACT_APP_API}/api/get_range_data/${date_range}`,
+      {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          authorization: 'Token ' + token,
+        },
+      }
+    );
     const response = await r.json();
     return response;
   } catch (err) {
