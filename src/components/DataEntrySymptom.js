c080e7df1f9fe21eb68bbe2e24d468b1eab02caf
jeffmoger <jeff-github@moger.com>
2020-09-28 14:56:12 -0700

    refactored to call api from external file

--- src/components/DataEntrySymptom.js
+++ src/components/DataEntrySymptom.js
@@ -1,16 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import DisplaySymptomSelect from './DisplaySymptomSelect';
-
-const useStyles = makeStyles({
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    maxWidth: '400px',
-    marginTop: 50,
-  },
-});
+import { getSymptomList } from '../functions/apiCalls';
 
 export default function DataEntrySymptom({
   authTokens,
@@ -19,26 +9,9 @@ export default function DataEntrySymptom({
   setDisabled,
   setNote,
 }) {
-  const classes = useStyles();
   const { id, token } = authTokens;
   const [displayList, setDisplayList] = useState([]);
   const [symptom, setSymptom] = useState('');
-
-  const getList = async (id, token) => {
-    try {
-      let res = await fetch(`${process.env.REACT_APP_API}/api/symptoms/read`, {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json',
-          authorization: 'Token ' + token,
-          id: id,
-        },
-      });
-      return await res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   const handleCheckmark = (e) => {
     let updateList = [...displayList];
@@ -51,12 +24,8 @@ export default function DataEntrySymptom({
     setNote(e.currentTarget.value);
   };
 
-  const handleSlider = (e) => {
-    //
-  };
-
   useEffect(() => {
-    getList(id, token).then((result) => {
+    getSymptomList(token).then((result) => {
       const showList = result.filter((item) => item.show === true);
       const newList = showList.map((item) => {
         item.selected = false;
@@ -93,7 +62,6 @@ export default function DataEntrySymptom({
         handleSliderChange={handleSliderChange}
         handleCheckmark={handleCheckmark}
         handleNotes={handleNotes}
-        handleSlider={handleSlider}
         symptom={symptom}
       />
     </div>
