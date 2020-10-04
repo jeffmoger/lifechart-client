88da5f33f6b8da0de85c6d01f287c99b50923ade
jeffmoger <jeff-github@moger.com>
2020-10-03 22:28:50 -0700

    added fetchauth and get profile

--- src/functions/apiCalls.js
+++ src/functions/apiCalls.js
@@ -12,3 +12,37 @@ export const getSymptomList = async (token) => {
     console.log(error);
   }
 };
+
+export async function fetchAuthentication(email, password) {
+  try {
+    const r = await fetch(`${process.env.REACT_APP_API}/api/users/login`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          email: email,
+          password: password,
+        },
+      }),
+    });
+    const response = await r.json();
+    return response;
+  } catch (err) {
+    return console.log(err);
+  }
+}
+
+export async function getProfile(id, token) {
+  const r = await fetch(`${process.env.REACT_APP_API}/api/users/read`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+      authorization: 'Token ' + token,
+      id: id,
+    },
+  });
+  const response = await r.json();
+  return response;
+}
