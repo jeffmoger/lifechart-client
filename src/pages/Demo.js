ecd8d6bde4e7b11709c879aaf2da5b81af7ee03c
jeffmoger <jeff-github@moger.com>
2020-10-03 22:24:59 -0700

    added demo page

---
+++ src/pages/Demo.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import HomeDemo from '../components/HomeDemo';
+import { fetchAuthentication, getProfile } from '../functions/apiCalls';
+
+const user = {
+  jeff: {
+    email: 'jeff@moger.com',
+    password: 'zxcv12',
+  },
+  demo: {
+    email: 'jeff-lifechart@moger.com',
+    password: 'BJTgeu7AZeqSarD',
+  },
+};
+export default function Demo() {
+  const [profile, setProfile] = useState('');
+  const [authTokens, setAuthTokens] = useState('');
+
+  useEffect(() => {
+    fetchAuthentication(user.jeff.email, user.jeff.password).then((result) => {
+      if (result.user) {
+        const { user } = result;
+        setAuthTokens({
+          id: user.id,
+          token: user.token,
+        });
+      }
+    });
+  }, []);
+  useEffect(() => {
+    fetchAuthentication(user.demo.email, user.demo.password).then((result) => {
+      getProfile(result.user.id, result.user.token).then((result) =>
+        setProfile(result)
+      );
+    });
+  }, []);
+  return (
+    <main>
+      <HomeDemo authTokens={authTokens} profile={profile} />
+    </main>
+  );
+}
