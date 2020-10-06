3747540eeaaabf00b4b8cb12cec7312ae5316e2b
jeffmoger <jeff-github@moger.com>
2020-10-05 22:02:08 -0700

    calling homecharts directly, no need for homeDemo

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import HomeDemo from '../components/HomeDemo';
-import { fetchAuthentication, getProfile } from '../functions/apiCalls';
+import HomeCharts from '../components/HomeCharts';
+import {
+  getDemoID,
+  fetchAuthentication,
+  getProfile,
+} from '../functions/apiCalls';
 
-const user = {};
 export default function Demo() {
   const [profile, setProfile] = useState('');
   const [authTokens, setAuthTokens] = useState('');
 
   useEffect(() => {
-    fetchAuthentication(user.jeff.email, user.jeff.password).then((result) => {
-      if (result.user) {
-        const { user } = result;
-        setAuthTokens({
-          id: user.id,
-          token: user.token,
+    getDemoID().then((user) => {
+      fetchAuthentication(user.email, user.password).then((result) => {
+        setAuthTokens(result.user);
+        getProfile(result.user.id, result.user.token).then((result) => {
+          setProfile(result);
         });
-      }
+      });
     });
   }, []);
-  useEffect(() => {
-    fetchAuthentication(user.demo.email, user.demo.password).then((result) => {
-      getProfile(result.user.id, result.user.token).then((result) =>
-        setProfile(result)
-      );
-    });
-  }, []);
+
   return (
     <main>
-      <HomeDemo authTokens={authTokens} profile={profile} />
+      {authTokens ? (
+        <HomeCharts authTokens={authTokens} profile={profile} />
+      ) : (
+        <div></div>
+      )}
     </main>
   );
 }
