728715879cb3e72c11179dc7e6e8c618696ec396
jeffmoger <jeff-github@moger.com>
2020-10-20 13:27:15 -0700

    added snackbar for error display to login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,8 @@ import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import { useAuth } from '../context/auth';
 
@@ -14,9 +15,8 @@ const useStyles = makeStyles((theme) => ({
       width: '35ch',
     },
   },
-  grid: {
-    flexGrow: 1,
-    marginLeft: 15,
+  submit: {
+    marginTop: 30,
   },
 }));
 
@@ -24,73 +24,86 @@ const Login = (props) => {
   const classes = useStyles();
 
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const [isError, setIsError] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [open, setOpen] = useState(false);
   const { setAuthTokens } = useAuth();
-  console.log('Rendering LoginForm');
+  const [severity, setSeverity] = useState('');
+  const [message, setMessage] = useState('');
 
   function handleSubmit(event) {
+    event.preventDefault();
     fetchAuthentication(email, password)
       .then((response) => {
+        if (response.errors) {
+          if (response.errors.length === 1) {
+            setMessage(response.errors[0].msg);
+          } else {
+            setMessage('Server side validation errors.');
+            //TODO: parse multiple errors and display with multiple snackbars. (HT-197)
+          }
+          setSeverity('error');
+          setOpen(true);
+        }
         if (response.user) {
           setAuthTokens(response.user);
           setLoggedIn(true);
-        } else {
-          setIsError(true);
         }
       })
       .catch((e) => {
-        setIsError(true);
         console.log(e);
       });
-    event.preventDefault();
   }
 
-  function Error() {
-    return <pre style={{ color: 'red' }}>TODO: error handling needed.</pre>;
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
   }
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   if (!isLoggedIn) {
     return (
       <main>
-        <div className={classes.grid}>
-          <Grid container justify="flex-start" alignItems="center" spacing={3}>
-            <Grid item xs={6}>
-              <form
-                onSubmit={handleSubmit}
-                className={classes.root}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField
-                  id="email"
-                  label="Email"
-                  type="text"
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-                <br />
-                <TextField
-                  id="password"
-                  label="Password"
-                  type="password"
-                  autoComplete="current-password"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-                <br />
-                <br />
-                <Button variant="contained" color="primary" type="submit">
-                  Login
-                </Button>
-              </form>
-              {isError ? <Error /> : null}
-            </Grid>
-          </Grid>
+        <div className={classes.root}>
+          <form onSubmit={handleSubmit} className={classes.root} noValidate>
+            <TextField
+              id="email"
+              label="Email"
+              type="text"
+              autoComplete="username"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              className={classes.submit}
+            >
+              Login
+            </Button>
+          </form>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity={severity}>
+              {message}
+            </Alert>
+          </Snackbar>
         </div>
       </main>
     );
