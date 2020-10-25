2fb909494aa48f60f9b857786c30cdf2bd8dd9f5
jeffmoger <jeff-github@moger.com>
2020-10-24 23:20:25 -0700

    removed profile calls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useAuth } from '../context/auth';
 import HomeCharts from '../components/HomeCharts';
-//import HomeDemo from '../components/HomeDemo';
 
 export default function Home() {
-  const isAuthenticated = useAuth();
-  const [profile, setProfile] = useState('');
-
-  useEffect(() => {
-    async function getProfile(id, token) {
-      const r = await fetch(`${process.env.REACT_APP_API}/api/users/read`, {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json',
-          authorization: 'Token ' + token,
-          id: id,
-        },
-      });
-      const response = await r.json();
-      return response;
-    }
-    if (isAuthenticated.authTokens) {
-      const { id, token } = isAuthenticated.authTokens;
-      getProfile(id, token).then((result) => setProfile(result));
-    }
-  }, [isAuthenticated.authTokens]);
-
+  const { authTokens } = useAuth();
   return (
     <main>
-      {isAuthenticated.authTokens ? (
-        <HomeCharts authTokens={isAuthenticated.authTokens} profile={profile} />
+      {authTokens ? (
+        <HomeCharts authTokens={authTokens} />
       ) : (
         <Redirect to={'/demo'} />
       )}
