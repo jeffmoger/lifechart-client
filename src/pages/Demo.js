2b14410f707bb7db87024c6ebdd3bae84d9b01c1
jeffmoger <jeff-github@moger.com>
2020-10-24 23:20:39 -0700

    removed profile calls

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import HomeCharts from '../components/HomeCharts';
-import {
-  getDemoID,
-  fetchAuthentication,
-  getProfile,
-} from '../functions/apiCalls';
+import { getDemoID, fetchAuthentication } from '../functions/apiCalls';
 
 export default function Demo() {
-  const [profile, setProfile] = useState('');
   const [authTokens, setAuthTokens] = useState('');
 
   useEffect(() => {
     getDemoID().then((user) => {
       fetchAuthentication(user.email, user.password).then((result) => {
         setAuthTokens(result.user);
-        getProfile(result.user.id, result.user.token).then((result) => {
-          setProfile(result);
-        });
       });
     });
   }, []);
@@ -24,7 +16,7 @@ export default function Demo() {
   return (
     <main>
       {authTokens ? (
-        <HomeCharts authTokens={authTokens} profile={profile} demo="demo" />
+        <HomeCharts authTokens={authTokens} demo="demo" />
       ) : (
         <div></div>
       )}
