5192e82f5ede030707a40257565f20874407c733
jeffmoger <jeff-github@moger.com>
2020-11-01 22:07:31 -0800

    testing

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -24,6 +24,7 @@ const menuItems = [
   ['About', '/about', true, false],
   ['Register', '/register', true, false],
   ['Settings', '/settings', false, true],
+  ['Auth', '/auth/google/redirect', true, false],
 ];
 
 const selectMenuItem = (menuItems, isAuth) => {
