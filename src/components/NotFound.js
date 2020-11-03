100b1580dd21237db2c2fac353299f2f9bfc1bad
jeffmoger <jeff-github@moger.com>
2020-11-02 22:15:13 -0800

    changed h1 to h3

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <div>
-      <h1>404 - Not Found!</h1>
+      <h3>404 - Not Found!</h3>
       <Link to="/">Go Home</Link>
     </div>
   );
