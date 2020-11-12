481622bdec89ba3f4a2182022992a39d2e752ee6
jeffmoger <jeff-github@moger.com>
2020-11-12 10:05:23 -0800

    removed console.log

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -13,7 +13,6 @@ export default async function getData(token, date_range, dataSourceIds) {
       }
     );
     const response = await r.json();
-    console.log(response);
     return response;
   } catch (err) {
     console.log(err);
