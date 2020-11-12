267e43f59030263c28ab1f0fca2a063f3cccb942
jeffmoger <jeff-github@moger.com>
2020-11-12 10:11:28 -0800

    light theme support

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -7,7 +7,6 @@ import Container from '@material-ui/core/Container';
 import './App.css';
 
 import PrivateRoute from './components/PrivateRoute';
-//import Nav from './components/Nav'
 import Home from './pages/Home';
 import Settings from './pages/Settings';
 import Login from './pages/Login';
@@ -23,38 +22,56 @@ import AppBar from './components/AppBar';
 
 function App() {
   const existingTokens = JSON.parse(localStorage.getItem('tokens'));
+  const themePreference = JSON.parse(localStorage.getItem('prefersDarkMode'));
   const [authTokens, setAuthTokens] = useState(existingTokens);
+  const [prefersDarkMode, setDarkMode] = useState(themePreference);
 
   const setTokens = (data) => {
     localStorage.setItem('tokens', JSON.stringify(data));
     setAuthTokens(data);
   };
 
-  const darkTheme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#5D4E8C',
-      },
-      type: 'dark',
-    },
-    typography: {
-      fontFamily: [
-        'Raleway',
-        'HelveticaNeue',
-        '"Helvetica Neue"',
-        'Helvetica',
-        'Arial',
-        'sans-serif',
-      ].join(','),
-    },
-  });
+  const toggleTheme = () => {
+    localStorage.setItem('prefersDarkMode', JSON.stringify(!prefersDarkMode));
+    setDarkMode(!prefersDarkMode);
+  };
+
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          primary: {
+            main: '#5D4E8C',
+          },
+          secondary: {
+            main: '#82ca9d',
+          },
+          error: {
+            main: '#f44336',
+          },
+          warning: {
+            main: '#ff9800',
+          },
+          info: {
+            main: '#2196f3',
+          },
+          success: {
+            main: '#4caf50',
+          },
+          type: prefersDarkMode ? 'dark' : 'light',
+          contrastThreshold: 3,
+          tonalOffset: 0.2,
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   return (
     <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          <AppBar />
+          <AppBar toggleTheme={toggleTheme} />
           <Container fixed>
             <Switch>
               <Route exact path="/" component={Home} />
