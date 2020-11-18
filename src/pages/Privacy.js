64ffb7e17628a50da2d6b4da32359222b7de3052
jeffmoger <jeff-github@moger.com>
2020-11-18 10:40:02 -0800

    privacy page added

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import PrivacyText from '../components/PrivacyText';
 
 export default function Privacy() {
   return (
-    <main className="terms">
-      <PrivacyText />
+    <main className="privacy">
+      <Container>
+        <PrivacyText />
+      </Container>
     </main>
   );
 }
