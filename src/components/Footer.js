e294316194b25b033859008dbefe32e5b6dfd4dc
jeffmoger <jeff-github@moger.com>
2020-11-18 12:41:15 -0800

    footer background theme color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,18 @@ const useStyles = makeStyles((theme) => ({
         : theme.palette.secondary.light,
     textDecoration: 'none',
   },
+  footer: {
+    backgroundColor: () => (theme.palette.type === 'light' ? '#EEE' : '$333'),
+    borderTop: () =>
+      theme.palette.type === 'light' ? '1px solid #FFF' : '1px solid $444',
+  },
 }));
 
 export default function Footer(props) {
   const classes = useStyles();
   const { toggleTheme } = props;
   return (
-    <footer>
+    <footer className={classes.footer}>
       <div>
         <Link to="/privacy" className={classes.link}>
           privacy
