3bfa34c1e348edc8342987a044c591ef1d2a0f72
jeffmoger <jeff-github@moger.com>
2020-11-19 22:40:08 -0800

    Changes to support chart and fade animations

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: 20,
+  },
   link: {
     color: () =>
       theme.palette.type === 'light'
@@ -17,150 +20,119 @@ const useStyles = makeStyles((theme) => ({
     display: 'none',
   },
   h2: {
-    marginBottom: 20,
+    marginBottom: 10,
+    color: () =>
+      theme.palette.type === 'light'
+        ? theme.palette.secondary.dark
+        : theme.palette.secondary.light,
   },
   p: {
     marginBottom: 20,
   },
 }));
-export default function Intro() {
+export default function Intro(props) {
   const classes = useStyles();
   return (
-    <Container maxWidth="md">
-      <Typography
-        variant="h6"
-        component="h2"
-        gutterBottom
-        className={classes.h2}
-      >
-        The smart way to chart your health and fitness data
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        LifeChart is a personal health and fitness tracking and charting app for
-        visualizing all of your health and fitness data from a single dashboard.
-        Many specialized apps offer individual health, fitness and nutrition
-        tracking capabilities that can provide excellent insights and encourage
-        healthy habits. Unfortunately, when this data remains siloed, it loses
-        much of its potential value.
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        LifeChart connects to your Google Fitness data and adds an additional
-        layer of tracking for mood and other user defined categories. With the
-        ability to see all of your information in one place, LifeChart’s custom
-        chart creation and rich data visualizations can help you identify
-        patterns and connections, offer actionable insight, and encourage a more
-        holistic understanding of your overall well being. Our goal is to give
-        you a more complete picture of how all of your inputs (nutrition, sleep,
-        activity) work together to affect and shape the way you feel.
-      </Typography>
-      <Typography
-        variant="h6"
-        component="h2"
-        gutterBottom
-        className={classes.h2}
-      >
-        Benefits
-      </Typography>
-      <Typography
-        variant="h6"
-        component="h3"
-        gutterBottom
-        className={classes.h2}
-      >
-        Weight Loss / Management
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        View rich visualizations of your calories consumed versus calories
-        burned. Calorie intake is also broken down into detailed nutrition
-        information allowing granular tracking of specific inputs (sugar,
-        sodium, trans fats).
-      </Typography>
-      <Typography
-        variant="h6"
-        component="h3"
-        gutterBottom
-        className={classes.h2}
-      >
-        Chronic Symptom Tracking
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        Recurring symptoms that don’t respond to occasional visits to the family
-        doctor can be frustrating. Wouldn’t it be great if you could generate a
-        chart with the occurrence of symptoms against a background of nutrition,
-        activity and sleep data for the last six months to take with you to the
-        doctor? LifeChart gives you this, and more.
-      </Typography>
-      <Typography
-        variant="h6"
-        component="h3"
-        gutterBottom
-        className={classes.h2}
-      >
-        Mood and Wellness
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        Mood tracking doesn’t get the attention that fitness and nutrition do,
-        perhaps because mood feels too subjective to accurately measure. At the
-        end of the day, our mood is the ultimate indicator of whether all our
-        efforts to eat and exercise better was worth it. LifeChart provides
-        easy, intuitive mood data entry and then charts that against your other
-        categories of health data.
-      </Typography>
+    <Fade in={true} timeout={3000}>
+      <div>
+        <Typography variant="body1" className={classes.p}>
+          LifeChart is a personal health and fitness tracking and charting app
+          for visualizing all of your health and fitness data from a single
+          dashboard. Many specialized apps offer individual health, fitness and
+          nutrition tracking capabilities that can provide excellent insights
+          and encourage healthy habits. Unfortunately, when this data remains
+          siloed, it loses much of its potential value.
+        </Typography>
+        <Typography variant="body1" className={classes.p}>
+          LifeChart connects to your Google Fitness data and adds an additional
+          layer of tracking for mood and other user defined categories. With the
+          ability to see all of your information in one place, LifeChart’s
+          custom chart creation and rich data visualizations can help you
+          identify patterns and connections, offer actionable insight, and
+          encourage a more holistic understanding of your overall well being.
+          Our goal is to give you a more complete picture of how all of your
+          inputs (nutrition, sleep, activity) work together to affect and shape
+          the way you feel.
+        </Typography>
+        <div style={{ display: '' }}>
+          <Typography variant="h5" component="h2" className={classes.slogan}>
+            Benefits
+          </Typography>
+          <Typography variant="h6" component="h3" className={classes.h2}>
+            Weight Loss / Management
+          </Typography>
+          <Typography variant="body1" className={classes.p}>
+            View rich visualizations of your calories consumed versus calories
+            burned. Calorie intake is also broken down into detailed nutrition
+            information allowing granular tracking of specific inputs (sugar,
+            sodium, trans fats).
+          </Typography>
+          <Typography variant="h6" component="h3" className={classes.h2}>
+            Chronic Symptom Tracking
+          </Typography>
+          <Typography variant="body1" className={classes.p}>
+            Recurring symptoms that don’t respond to occasional visits to the
+            family doctor can be frustrating. Wouldn’t it be great if you could
+            generate a chart with the occurrence of symptoms against a
+            background of nutrition, activity and sleep data for the last six
+            months to take with you to the doctor? LifeChart gives you this, and
+            more.
+          </Typography>
+          <Typography variant="h6" component="h3" className={classes.h2}>
+            Mood and Wellness
+          </Typography>
+          <Typography variant="body1" className={classes.p}>
+            Mood tracking doesn’t get the attention that fitness and nutrition
+            do, perhaps because mood feels too subjective to accurately measure.
+            At the end of the day, our mood is the ultimate indicator of whether
+            all our efforts to eat and exercise better was worth it. LifeChart
+            provides easy, intuitive mood data entry and then charts that
+            against your other categories of health data.
+          </Typography>
 
-      <Typography
-        variant="h6"
-        component="h3"
-        gutterBottom
-        className={classes.h2}
-      >
-        Motivation to Change Habits and Behaviour
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        The very act of tracking health data tends to bring focus to healthy
-        habits. Indeed, lifeChart began as a simple project to bring better data
-        to a doctor appointment but the act of tracking my own data did so much
-        to improve my habits that a return to the doctor hasn’t been necessary.
-      </Typography>
+          <Typography variant="h6" component="h3" className={classes.h2}>
+            Motivation to Change Habits and Behaviour
+          </Typography>
+          <Typography variant="body1" className={classes.p}>
+            The very act of tracking health data tends to bring focus to healthy
+            habits. Indeed, lifeChart began as a simple project to bring better
+            data to a doctor appointment but the act of tracking my own data did
+            so much to improve my habits that a return to the doctor hasn’t been
+            necessary.
+          </Typography>
 
-      <Typography
-        variant="h6"
-        component="h3"
-        gutterBottom
-        className={classes.h2}
-      >
-        Quantified-Self Enthusiasts
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        LifeChart is designed to give you control over creating, combining and
-        over-laying your data to help you identify the trends or patterns unique
-        to you. LifeChart aspires to be flexible enough to support your
-        experiments, and we are very receptive to any feedback from the
-        community to make our platform more useful.
-      </Typography>
+          <Typography variant="h6" component="h3" className={classes.h2}>
+            Quantified-Self Enthusiasts
+          </Typography>
+          <Typography variant="body1" className={classes.p}>
+            LifeChart is designed to give you control over creating, combining
+            and over-laying your data to help you identify the trends or
+            patterns unique to you. LifeChart aspires to be flexible enough to
+            support your experiments, and we are very receptive to any feedback
+            from the community to make our platform more useful.
+          </Typography>
 
-      <Typography
-        variant="h6"
-        component="h2"
-        gutterBottom
-        className={classes.h2}
-      >
-        Getting Started
-      </Typography>
-      <Typography variant="body1" className={classes.p} gutterBottom>
-        To get started, login to LifeChart with your{' '}
-        <Link to="/login" className={classes.link}>
-          Google account
-        </Link>
-        . This gives LifeChart permission to read your activity and nutrition
-        data from Google Fit. If you do not have a GoogleFit account we do not
-        recommend creating an account at this time, as the primary value we
-        provide is the ability to layer additional data points on top of the
-        background fitness and nutrition you are already collecting. A{' '}
-        <Link to="/demo" className={classes.link}>
-          demo of a LifeChart dashboard can seen here
-        </Link>
-        .
-      </Typography>
-    </Container>
+          <Typography variant="h6" component="h2" className={classes.h2}>
+            Getting Started
+          </Typography>
+          <Typography variant="body1" className={classes.p}>
+            To get started, login to LifeChart with your{' '}
+            <Link to="/login" className={classes.link}>
+              Google account
+            </Link>
+            . This gives LifeChart permission to read your activity and
+            nutrition data from Google Fit. If you do not have a GoogleFit
+            account we do not recommend creating an account at this time, as the
+            primary value we provide is the ability to layer additional data
+            points on top of the background fitness and nutrition you are
+            already collecting. A{' '}
+            <Link to="/demo" className={classes.link}>
+              demo of a LifeChart dashboard can seen here
+            </Link>
+            .
+          </Typography>
+        </div>
+      </div>
+    </Fade>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,218 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Logo from '../components/Logo.js';
 import Intro from '../components/Intro';
+import ChartForHomePage from '../charts/ChartForHomePage';
 import { useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    marginTop: 10,
+    marginBottom: -30,
+  },
+  container: {
+    //paddingTop: 20,
+    //marginBottom: -20,
+  },
+  link: {
+    color: () =>
+      theme.palette.type === 'light'
+        ? theme.palette.secondary.dark
+        : theme.palette.secondary.main,
+    textDecoration: () =>
+      theme.palette.type === 'light' ? 'underline' : 'none',
+  },
+  slogan: {
+    marginBottom: 20,
+    marginTop: 20,
+    //color: () => (theme.palette.type === 'light' ? '#5D4E8C' : '#8884d8'),
+  },
+}));
+
+const headProps = (type) => {
+  const theme = {
+    light: {
+      start: 'rgb(220, 220, 220)',
+      end: 'rgb(250, 250, 250)',
+    },
+    dark: {
+      start: 'rgb(48, 48, 48)',
+      end: 'rgb(64, 64, 64)',
+    },
+  };
+  if (type === 'light') return [theme.light.start, theme.light.end];
+  if (type === 'dark') return [theme.dark.start, theme.dark.end];
+};
 
 const logoProps = (theme) => {
   const type = theme.palette.type;
   let lifeFill;
-  type === 'light' ? (lifeFill = '#5D4E8C') : (lifeFill = '#8884d8');
+  type === 'light' ? (lifeFill = '#5D4E8C') : (lifeFill = '#FFF');
   return {
     lifeFill,
     chartFill: '#82CA9D',
-    width: 390,
-    height: 120,
+    width: 390 / 1.2,
+    height: 120 / 1.2,
   };
 };
 
+const chartProps = (theme) => {
+  const type = theme.palette.type;
+  let fill;
+  let stroke;
+  type === 'light' ? (fill = '#dcdcdc') : (fill = '#303030');
+  type === 'light' ? (stroke = '#5D4E8C') : (stroke = '#8884d8');
+  return {
+    stroke,
+    fill,
+  };
+};
+
+const initHeadColor = (type) => {
+  let arr = headProps(type);
+  return arr[0];
+};
+
+const generateData = (min, max) => {
+  const arr = [];
+  const chartLength = 5;
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  var i;
+  for (i = 0; i < chartLength; i++) {
+    arr.push({ a: 1, b: 10 });
+    arr.push({ a: getRandomIntInclusive(min, max), b: 10 });
+  }
+  arr.push({ a: 1, b: 10 });
+  return arr;
+};
+
 export default function Home(props) {
+  const classes = useStyles();
   const theme = useTheme();
+  const [data, setData] = useState(() => generateData(1, 1));
+  const [showText, setShowText] = useState(false);
+  const [textFlag, setTextFlag] = useState(false);
+  const [subtitle, setSubtitle] = useState(false);
+  const [headArr, setHeadArr] = useState(() => headProps(theme.palette.type));
+  const [headColor, setHeadColor] = useState(() =>
+    initHeadColor(theme.palette.type)
+  );
+
+  useEffect(() => {
+    console.log(`TYPE: ${theme.palette.type}`);
+    setHeadColor(initHeadColor(theme.palette.type));
+    setHeadArr(headProps(theme.palette.type));
+  }, [theme.palette.type]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setData(generateData(2, 10));
+      setSubtitle(true);
+      setTextFlag(true);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (textFlag) {
+      const timer = setTimeout(() => {
+        setShowText(true);
+
+        const sleep = (milliseconds) => {
+          return new Promise((resolve) => setTimeout(resolve, milliseconds));
+        };
+
+        const fadeIn = async () => {
+          const parseRGB = (rgb) => {
+            return rgb.substring(
+              rgb.lastIndexOf('(') + 1,
+              rgb.lastIndexOf(')')
+            );
+          };
+
+          const rgbStart = parseRGB(headArr[0]);
+          const rgbEnd = parseRGB(headArr[1]);
+
+          const rgbStartArr = rgbStart.split(',');
+          const rgbEndArr = rgbEnd.split(',');
+
+          const start = rgbStartArr[0];
+          const end = rgbEndArr[0];
+          console.log(`start: ${start}`);
+          console.log(`end: ${end}`);
+          let newRGB;
+
+          if (start < end) {
+            let sleepLength = Math.round(500 / (end - start));
+            console.log(`sleep: ${sleepLength}`);
+            for (let i = start; i <= end; i++) {
+              await sleep(sleepLength);
+              newRGB = `rgb(${i}, ${i}, ${i})`;
+              setHeadColor(newRGB);
+            }
+          }
+          if (start > end) {
+            let sleepLength = Math.round(500 / (start - end));
+            console.log(`sleep: ${sleepLength}`);
+            for (let i = start; i >= end; i--) {
+              await sleep(sleepLength);
+              newRGB = `rgb(${i}, ${i}, ${i})`;
+              setHeadColor(newRGB);
+            }
+          }
+          console.log(`newRGB: ${newRGB}`);
+        };
+        fadeIn();
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [headArr, textFlag, theme.palette.type]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setData(generateData(2, 10));
+    }, 10000);
+    setTimeout(() => {
+      clearInterval(timer);
+    }, 500000);
+  }, []);
+
   return (
     <>
-      <header>
-        <Container maxWidth="md">
+      <header style={{ textAlign: 'left', backgroundColor: headColor }}>
+        <Container maxWidth="md" className={classes.logo}>
           <Logo logoProps={logoProps(theme)} />
         </Container>
+        <div
+          className={`homechart-${theme.palette.type}`}
+          style={{ backgroundColor: headColor }}
+        >
+          <ChartForHomePage data={data} colors={chartProps(theme)} />
+        </div>
       </header>
-      <main>
-        <Intro toggleTheme={props.toggleTheme} />
+
+      <main className={`homechart-${theme.palette.type}`}>
+        <Container maxWidth="md">
+          <Fade in={subtitle} timeout={2000}>
+            <Typography
+              variant="h5"
+              component="h2"
+              gutterBottom
+              className={classes.slogan}
+              align="left"
+            >
+              The smart way to chart your health and fitness data
+            </Typography>
+          </Fade>
+          {showText && <Intro toggleTheme={props.toggleTheme} fade={true} />}
+        </Container>
       </main>
     </>
   );
