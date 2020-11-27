3b1406a36aa224a31434abe96155f346e92c2776
jeffmoger <jeff-github@moger.com>
2020-11-27 15:31:08 -0800

    asded default parameter to startTodaySeconds

--- src/functions/dateFunctions.js
+++ src/functions/dateFunctions.js
@@ -20,8 +20,8 @@ export const dateMillis = (date) => {
   return parseInt(moment(date).format('x'), 10);
 };
 
-export const startTodaySeconds = () => {
-  const date = new Date();
+export const startTodaySeconds = (date = new Date()) => {
+  //const date = new Date();
   const start = moment(date).startOf('day');
   return parseInt(moment(start).format('X'), 10);
 };
