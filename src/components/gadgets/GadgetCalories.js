4fe785706194bc24ab874e8725f5f859458d8fa5
jeffmoger <jeff-github@moger.com>
2020-12-02 22:19:42 -0800

    refactored for reuse

--- src/components/gadgets/GadgetCalories.js
+++ src/components/gadgets/GadgetCalories.js
@@ -1,25 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import GadgetRing from './GadgetRing';
 import { createCalorieGadget } from './functions';
+import { calorieGadgets } from './config';
 
 const GadgetCalories = (props) => {
-  const { data, label, goal, color, days, includeToday, type } = props;
-  const [score, setScore] = useState(0);
+  const { data, color } = props;
+  const [score, setScore] = useState([]);
 
   useEffect(() => {
     if (data) {
-      setScore(createCalorieGadget(data, days, includeToday, type));
+      let arr = [];
+      calorieGadgets.map((gadget) =>
+        arr.push(
+          createCalorieGadget(
+            data,
+            gadget.days,
+            gadget.includeToday,
+            gadget.type
+          )
+        )
+      );
+      setScore(arr);
     }
-  }, [data, days, includeToday, type]);
-
-  useEffect(() => {
-    //console.log(data);
   }, [data]);
 
   return (
-    <div>
-      <GadgetRing score={score} color={color} goal={goal} label={label} />
-    </div>
+    <>
+      {score.length > 0
+        ? calorieGadgets.map((gadget, index) => (
+            <div key={gadget.name}>
+              <GadgetRing
+                score={score[index]}
+                color={color}
+                goal={gadget.goal}
+                label={gadget.label}
+              />
+            </div>
+          ))
+        : null}
+    </>
   );
 };
 
