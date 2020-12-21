e9c6bf6b4ced777824bf8fd63c5925c36d0625ad
jeffmoger <jeff-github@moger.com>
2020-12-20 18:49:11 -0800

    Fixed styles

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 export default function NotFound() {
   return (
     <main>
-      <Container>
-        <Typography variant="h6" component="h2" gutterBottom>
+      <Container maxWidth="md" style={{ marginTop: 40 }}>
+        <Typography variant="h4" component="h2" gutterBottom>
           404 Not Found
         </Typography>
-        <Link to="/">Go Home</Link>
+        <Typography variant="body1" gutterBottom>
+          <Button component={Link} to="/">
+            Go Home
+          </Button>
+        </Typography>
       </Container>
     </main>
   );
