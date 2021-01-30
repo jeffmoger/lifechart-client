1afe52f8a645f383b330e956e9c00b8dca12b274
jeffmoger <jeff-github@moger.com>
2021-01-30 12:28:28 -0800

    import react-ga

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-N827597BNJ');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <React.Fragment>
     <Router>
